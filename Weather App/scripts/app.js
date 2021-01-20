b967d904ac6f4e0e299f89c4a1083e4086f60c77
Adriani Kao <adrianikao119@gmail.com>
2021-01-21 00:35:44 +0800

    html template/css/js

---
+++ Weather App/scripts/app.js
@@ -0,0 +1,29 @@
+const city = document.querySelector('.change-location');
+
+const updateCity = async(city)=>{
+    const cityDets = await getCity(city);
+    //coz the forcast.js is behind the app.js, it's able to call function we set in forecast .js
+    const weather = await getweather(cityDets.Key);
+
+    return{
+        cityDets:cityDets,
+        weather:weather,
+    };
+};
+
+city.addEventListener('submit', e=>{
+    //prevent default action
+    e.preventDefault();
+
+    //get city value
+// console.log(city.city);
+    const location = city.city.value.trim();
+    city.reset();
+
+    updateCity(location)
+    .then(data=>console.log(data))
+    .catch(err => console.log(err));
+});
+
+
+
---
+++ Weather App/scripts/forcast.js
@@ -0,0 +1,42 @@
+const key = 'FKgqsCn04iUySlgPC3M69qS0KqV4gwD5';
+//Location API give us a keycode then that key code make a request to weather API to get city info.
+
+
+//get weather info 
+
+const getweather = async(id)=>{
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data =  await response.json();
+
+    return data[0];
+
+};
+
+
+//get city info
+const getCity = async(city)=>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    //the website we wanna make request for
+
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+   
+};
+
+getCity('osaka')
+.then(data=> {
+   return getweather(data.Key);
+}).then(data=> console.log(data))
+.catch(err => console.log(err));
+
+// getweather('2421828')
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
