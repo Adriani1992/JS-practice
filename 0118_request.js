7481d8ae2c33a1ee2c014126b58fccdfb63300c4
Adriani Kao <adrianikao119@gmail.com>
2021-01-20 16:17:36 +0800

    practice

--- 0118_request.js
+++ 0118_request.js
@@ -1,37 +1,47 @@
-const getTodos =(callback)=> {
-const request = new XMLHttpRequest();
+const getTodos =(resource)=> {
+    return new Promise((resolve, reject)=>{
+        const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange',()=>{
-    // console.log(request,request.readyState);
-    //readState is return the state an XMLHttpRequest client is in.
-    if(request.readyState=== 4 && request.status ===200){
-        const data = JSON.parse(request.responseText);
-        callback(undefined, data);
-    }else if(request.readyState ===4){
-        callback('could not fetch data', undefined)
-    }
-    //0 means unsent 
-    //1 means opened => open()has been called
-    //2 means HEADERS_RECEIVED => send() has been called and headers and status are available.
-    //3 means LOADING => Downloading; responseText holds partial data.
-    //4 means DONE => The operation is complete.
-})
+        request.addEventListener('readystatechange', () => {
+            // console.log(request,request.readyState);
+            //readState is return the state an XMLHttpRequest client is in.
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('could not fetch data', undefined);
+            }
+            //0 means unsent 
+            //1 means opened => open()has been called
+            //2 means HEADERS_RECEIVED => send() has been called and headers and status are available.
+            //3 means LOADING => Downloading; responseText holds partial data.
+            //4 means DONE => The operation is complete.
+        })
 
 
 
-request.open('GET','https://jsonplaceholder.typicode.com/todos/')
-//'GET' is the type of request we want to make
-//'URL' is the endpoints we get the data from
+        request.open('GET', resource)
+        //'GET' is the type of request we want to make
+        //'URL' is the endpoints we get the data from
 
-request.send()
-//send the request!!
+        request.send()
+        //send the request!!
+    });
+
+
+
+// getTodos((err,data)=>{
+//     console.log('callback fired');
+//     if(err){
+//     console.log(err);
+//     }else{
+//         console.log(data);
+//     }
+// });
 };
 
-getTodos((err,data)=>{
-    console.log('callback fired');
-    if(err){
-    console.log(err);
-    }else{
-        console.log(data);
-    }
-});
+getTodos('0118_sample.json').then(data=>{
+    console.log('promise resolved:', data);
+}).catch(err=>{
+    console.log('Promise reject:', err);
+})
---
+++ 0119_async.js
@@ -0,0 +1,25 @@
+const getTodos = async ()=>{
+
+    const response = await fetch('0118_sample.json');
+    //await means it will wait till fetch get resolved result back, then saved it into response variable.
+    //it won't block the resr of  code
+    // if(response.status !== 200){
+    //     throw new Error('cannot fetch the data');
+    // }
+
+    const data = response.json()
+    //access the data in json, then assign it into data.
+    return data;
+
+    //only blocking the data inside the async function
+};
+
+
+getTodos()
+.then((data)=>{
+    console.log('resolved:', data)
+})
+.catch(err=> console.log('rejected:', err.message))
+
+//async return a promise.
+//the code outside the function won't be blocked.
